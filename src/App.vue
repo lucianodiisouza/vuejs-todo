8ebdf82c34fc8462d4968d01637188d83c33d8ae
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-05-29 20:11:54 -0300

    feat: just take a look on how life-cycle methods work

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,24 @@
 <template>
   <div>
-    <h1>TaskList</h1>
-    <button @click="handleShowHideList">Ver a lista</button>
-    <br />
-
-    <input
-      type="text"
-      v-model="state.currentTask"
-      placeholder="Digite sua task"
-      @keyup.enter="addTask"
-      v-focus
-    />
-
-    <ul v-if="state.showList">
-      <li
-        v-for="(task, index) in state.tasks"
-        @dblclick="complete(task)"
-        class="task-item"
-        :key="`${task}-${index}`"
-        :class="{ 'line-through': task.isDone }"
-      >
-        {{ task.name }} <button @click="remove(task)">&times;</button>
-      </li>
-    </ul>
-    <p v-else>Lista de tafefas escondida.</p>
+    <h1>Vue</h1>
   </div>
 </template>
 
 <script>
-import { reactive } from "vue";
+import { onMounted } from "vue";
 
-const focus = {
-  inserted: (el) => {
-    el.focus();
-  },
-};
-
+// Composition API
 export default {
-  directives: {
-    focus,
-  },
   setup() {
-    const state = reactive({
-      showList: false,
-      currentTask: "",
-      tasks: [
-        { name: "Comprar leche", isDone: false },
-        { name: "Comprar pan", isDone: false },
-        { name: "Comprar huevos", isDone: false },
-        { name: "Comprar carne", isDone: false },
-      ],
+    onMounted(() => {
+      console.log("foi montado composition api");
     });
-
-    function remove(task) {
-      state.tasks = state.tasks.filter((t) => t.name !== task.name);
-    }
-
-    function complete(task) {
-      state.tasks = state.tasks.map((t) => {
-        if (t.name === task.name) {
-          return { ...t, isDone: !t.isDone };
-        }
-        return { ...t };
-      });
-    }
-
-    function handleShowHideList() {
-      state.showList = !state.showList;
-    }
-    
-    function addTask() {
-      state.tasks.push({ name: state.currentTask, isDone: false });
-      state.currentTask = "";
-    }
-
-    return { state, remove, complete, handleShowHideList, addTask };
   },
 };
+// Options API
+// export default {
+//   mounted() {
+//     console.log("foi montado Options API");
+//   },
+// };
 </script>
-<style scoped>
-.line-through {
-  text-decoration: line-through;
-}
-.task-item {
-  cursor: pointer;
-}
-</style>
